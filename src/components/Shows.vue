101e8652faeba820bed329a9618dda9a8088e0ca
pabloyanezb <pabloyanezbarriga@gmail.com>
2020-08-05 16:38:51 -0400

    ADD: Método para crear documento en base a un formulario

--- src/components/Shows.vue
+++ src/components/Shows.vue
@@ -2,8 +2,8 @@
   <div class="container">
     <div class="row">
       <div class="col s12 m12">
-        <h3 class="center-align">Shows:</h3>
-        <table class="striped">
+        <h3>All Shows:</h3>
+        <table class="striped card blue-grey lighten-4">
           <thead>
             <th>Title</th>
             <th>Network</th>
@@ -16,11 +16,57 @@
               <td>{{ show.title }}</td>
               <td>{{ show.network }}</td>
               <td class="center-align">{{ show.numberOfSeasons }}</td>
-              <td class="center-align">{{ show.isCurrent }}</td>
+              <td class="center-align">
+                <i class="material-icons" v-if="show.isCurrent">check</i>
+                <i class="material-icons" v-if="!show.isCurrent">close</i>
+              </td>
               <td>{{ show.genres.join(' / ') }}</td>
             </tr>
           </tbody>
         </table>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col s12 m12">
+        <h4>Add a Show</h4>
+        <form class="card blue-grey darken-4 white-text" @submit.prevent="addShow">
+          <div class="row">
+            <div class="input-field col m8">
+              <input id="title" type="text" class="validate" required v-model="title">
+              <label for="title">Title</label>
+            </div>
+            <div class="input-field col m4">
+              <input id="network" type="text" class="validate" required v-model="network">
+              <label for="network">Network</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col m8">
+              <input id="genres" type="text" class="validate" required v-model="genres">
+              <label for="genres">Genres</label>
+            </div>
+            <div class="input-field col m4">
+              <input id="numberOfSeasons" type="number" class="validate" required v-model="numberOfSeasons">
+              <label for="numberOfSeasons">Number of Seasons</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col m8 offset-m1">
+              Is Current:
+              <label>
+                <input name="isCurrent" type="radio" required checked v-model="isCurrent" :value="true"/>
+                <span>Yes</span>
+              </label>
+              <label>
+                <input name="isCurrent" type="radio" required v-model="isCurrent" :value="false"/>
+                <span>No</span>
+              </label>
+            </div>
+            <div class="col m3">
+              <button type="submit" class="waves-effect waves-light btn white-text">Add</button>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   </div>
@@ -32,7 +78,28 @@ export default {
   name: 'Shows',
   data() {
     return {
-      shows: []
+      shows: [],
+      title: '',
+      network: '',
+      numberOfSeasons: '',
+      isCurrent: null,
+      genres: []
+    }
+  },
+  methods: {
+    addShow() {
+      db.collection('shows').add({
+        title: this.title,
+        network: this.network,
+        numberOfSeasons: parseInt(this.numberOfSeasons),
+        isCurrent: this.isCurrent,
+        genres: this.genres.split(', ', 5)
+      })
+      this.title = '';
+      this.network = '';
+      this.numberOfSeasons = '';
+      this.isCurrent = null;
+      this.genres = [];
     }
   },
   firestore() {
@@ -52,18 +119,20 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+form {
+  padding: 1rem 2rem;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+input {
+  color: white;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.btn {
+  width: 100%;
 }
-a {
-  color: #42b983;
+form .row {
+  margin-bottom: 0;
+}
+span {
+  margin-left: 1rem;
 }
 </style>
